5f68c9e143260fe36991710eb98be2df9c69a702
paul-leonard <paul.ryan.leonard@gmail.com>
2020-11-13 19:40:38 -0800

    file structure completed and tests verify connections

---
+++ python/code_challenges/linked_list/linked_list/linked_list.py
@@ -0,0 +1,23 @@
+class LinkedList:
+  """
+  Put docstring here
+  """
+
+  def __init__(self):
+    # initialization here
+    #self.head = "empty"
+    pass
+
+  def some_method(self):
+    # method body here
+    pass
+
+# class Node(value):
+#   """
+#   sdfsdf
+#   """
+
+#   def __init__(self):
+#     self.value = value
+#     self.next_node_pointer = "null"
+
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,15 @@
-from linked_list.linked_list import LinkedList
 
+import pytest
 
-def test_import():
+from code_challenges.linked_list.linked_list.linked_list import LinkedList
+
+def test_LinkedList():
     assert LinkedList
+
+# def test_LinkedList_empty():
+#     actual = LinkedList().head
+#     expected = "empty"
+#     assert actual == expected
+
+
+
