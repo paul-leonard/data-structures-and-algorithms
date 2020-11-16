34c7f2edfc9e48e5ebac9f585185a917fec9bac1
paul-leonard <paul.ryan.leonard@gmail.com>
2020-11-15 22:35:33 -0800

    LinkedList __str__ created and passing tests

--- python/code_challenges/linked_list/linked_list/linked_list.py
+++ python/code_challenges/linked_list/linked_list/linked_list.py
@@ -1,11 +1,11 @@
 '''
 Required features:
-[x] Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
-[x] Within your LinkedList class, include a head property.
-[x] Upon instantiation, an empty Linked List should be created.
-[x] Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
-[x] Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
-[ ] Define a method called toString (or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
+- [x] Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
+- [x] Within your LinkedList class, include a head property.
+- [x] Upon instantiation, an empty Linked List should be created.
+- [x] Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
+- [x] Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
+- [x] Define a method called toString (or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
     - "{ a } -> { b } -> { c } -> NULL"
 '''
 
@@ -32,8 +32,18 @@ class LinkedList:
 
     return False
 
-  def __str__():
-    pass
+  def __str__(self):
+    if self.head == "empty":
+      return "NULL"
+
+    text_version = ""
+    current = self.head
+    while current.next_node != "empty":
+      text_version = text_version + "{ " + current.value + " } -> "
+      current = current.next_node
+
+    text_version = text_version + "{ " + current.value + " } -> NULL"
+    return text_version
 
 
 class Node():
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,12 +1,12 @@
 '''
 Required testing features:
-[x] Can successfully instantiate an empty linked list
-[x] Can properly insert into the linked list
-[x] The head property will properly point to the first node in the linked list
-[x] Can properly insert multiple nodes into the linked list
-[x] Will return true when finding a value within the linked list that exists
-[x] Will return false when searching for a value in the linked list that does not exist
-[ ] Can properly return a collection of all the values that exist in the linked list
+- [x] Can successfully instantiate an empty linked list
+- [x] Can properly insert into the linked list
+- [x] The head property will properly point to the first node in the linked list
+- [x] Can properly insert multiple nodes into the linked list
+- [x] Will return true when finding a value within the linked list that exists
+- [x] Will return false when searching for a value in the linked list that does not exist
+- [x] Can properly return a collection of all the values that exist in the linked list
 '''
 
 import pytest
