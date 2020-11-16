e4ba3457c22f4c06244151bcefb785f0afa7a190
paul-leonard <paul.ryan.leonard@gmail.com>
2020-11-15 21:12:25 -0800

    Linked List Insert method works correctly and passes tests

--- python/code_challenges/linked_list/linked_list/linked_list.py
+++ python/code_challenges/linked_list/linked_list/linked_list.py
@@ -1,3 +1,14 @@
+'''
+Required features:
+[x] Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
+[x] Within your LinkedList class, include a head property.
+[x] Upon instantiation, an empty Linked List should be created.
+[x] Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
+[ ] Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
+[ ] Define a method called toString (or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
+    - "{ a } -> { b } -> { c } -> NULL"
+'''
+
 class LinkedList:
   """
   Put docstring here
@@ -5,19 +16,21 @@ class LinkedList:
 
   def __init__(self):
     # initialization here
-    #self.head = "empty"
-    pass
+    self.head = "empty"
 
   def some_method(self):
     # method body here
     pass
 
-# class Node(value):
-#   """
-#   sdfsdf
-#   """
+  def insert(self, value):
+    self.head = Node(value, self.head)
 
-#   def __init__(self):
-#     self.value = value
-#     self.next_node_pointer = "null"
+class Node():
+  """
+  sdfsdf
+  """
+
+  def __init__(self, value, next_node):
+    self.value = value
+    self.next_node = next_node
 
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,15 +1,51 @@
+'''
+Required testing features:
+[x] Can successfully instantiate an empty linked list
+[x] Can properly insert into the linked list
+[x] The head property will properly point to the first node in the linked list
+[x] Can properly insert multiple nodes into the linked list
+[ ] Will return true when finding a value within the linked list that exists
+[ ] Will return false when searching for a value in the linked list that does not exist
+[ ] Can properly return a collection of all the values that exist in the linked list
+'''
 
 import pytest
 
-from code_challenges.linked_list.linked_list.linked_list import LinkedList
+from code_challenges.linked_list.linked_list.linked_list import LinkedList, Node
 
 def test_LinkedList():
-    assert LinkedList
+  assert LinkedList
 
-# def test_LinkedList_empty():
-#     actual = LinkedList().head
-#     expected = "empty"
-#     assert actual == expected
+def test_LinkedList_empty():
+  actual = LinkedList().head
+  expected = "empty"
+  assert actual == expected
 
+def test_insert():
+  test_list = LinkedList()
+  test_list.insert(3)
+  actual = test_list.head.value
+  expected = 3
+  assert actual == expected
 
+def test_multi_insert():
+  test_list = LinkedList()
+  test_list.insert(1)
+  test_list.insert(2)
+  test_list.insert(3)
+  test_list.insert(4)
+  actual = []
+  actual = [test_list.head.value, test_list.head.next_node.value, test_list.head.next_node.next_node.value, test_list.head.next_node.next_node.next_node.value]
+  expected = [4, 3, 2, 1,]
+  assert actual == expected
+
+def test_multi_insert():
+  test_list = LinkedList()
+  test_list.insert(1)
+  test_list.insert(2)
+  test_list.insert(3)
+  test_list.insert(4)
+  actual = test_list.head.value
+  expected = 4
+  assert actual == expected
 
