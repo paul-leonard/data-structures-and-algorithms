9ebb4d688e799dcdb0f611c05fa86f0d1501eba1
paul-leonard <paul.ryan.leonard@gmail.com>
2020-11-22 16:08:04 -0800

    create files and structure, list out features

--- python/README.md
+++ python/README.md
@@ -2,6 +2,23 @@
 
 ## Language: `Python`
 
+-----------------------------------------------------------------
+
+
+
+# Stacks and Queues
+<!-- Short summary or background information -->
+
+## Challenge
+<!-- Description of the challenge -->
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## API
+<!-- Description of each method publicly available to your Stack and Queue-->
+
+
 
 -----------------------------------------------------------------
 
---
+++ python/tests/stacks_and_queues/test_stacks_and_queues.py
@@ -0,0 +1,21 @@
+'''
+- [ ] Challenge 10 Required Testing Features:
+- [ ] Can successfully push onto a stack
+- [ ] Can successfully push multiple values onto a stack
+- [ ] Can successfully pop off the stack
+- [ ] Can successfully empty a stack after multiple pops
+- [ ] Can successfully peek the next item on the stack
+- [ ] Can successfully instantiate an empty stack
+- [ ] Calling pop or peek on empty stack raises exception
+- [ ] Can successfully enqueue into a queue
+- [ ] Can successfully enqueue multiple values into a queue
+- [ ] Can successfully dequeue out of a queue the expected value
+- [ ] Can successfully peek into a queue, seeing the expected value
+- [ ] Can successfully empty a queue after multiple dequeues
+- [ ] Can successfully instantiate an empty queue
+- [ ] Calling dequeue or peek on empty queue raises exception
+'''
+
+import pytest
+
+from code_challenges.linked_list.linked_list.linked_list import LinkedList
