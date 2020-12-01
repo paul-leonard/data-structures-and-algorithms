331791a86359cf23ff5ae2e0bb5c9267edb2f539
paul-leonard <paul.ryan.leonard@gmail.com>
2020-11-30 19:10:05 -0800

    5 tests passing and add method functioning

--- python/tests/test_tree.py
+++ python/tests/test_tree.py
@@ -1,7 +1,7 @@
 '''
 Challenge 16? (Binary Tree and BST Implementation) Required Testing Features:
-- [ ] Can successfully instantiate an empty tree
-- [ ] Can successfully instantiate a tree with a single root node
+- [x] Can successfully instantiate an empty tree
+- [x] Can successfully instantiate a tree with a single root node
 - [ ] Can successfully add a left child and right child to a single root node
 - [ ] Can successfully return a collection from a preorder traversal
 - [ ] Can successfully return a collection from an inorder traversal
@@ -10,7 +10,33 @@ Challenge 16? (Binary Tree and BST Implementation) Required Testing Features:
 
 import pytest
 
-from tree.tree import Node, BinaryTree
+from tree.tree import Node, BinaryTree, BinaryTreeSearch
 
 def test_connection():
     return Node()
+
+def test_empty_tree():
+    t = BinaryTree()
+    actual = t.root.value
+    expected = None
+    assert actual == expected
+
+def test_tree_of_one():
+    t = BinaryTree("all alone")
+    actual = t.root.value
+    expected = "all alone"
+    assert actual == expected
+
+def test_add_left_child():
+    t = BinaryTreeSearch(7)
+    t.add(5)
+    actual = t.root.left_node.value
+    expected = 5
+    assert actual == expected
+
+def test_add_right_child():
+    t = BinaryTreeSearch(5)
+    t.add(9)
+    actual = t.root.right_node.value
+    expected = 9
+    assert actual == expected
--- python/tree/tree.py
+++ python/tree/tree.py
@@ -1,11 +1,11 @@
 '''
 Challenge 16? (Binary Tree and BST Implementation) Required Features:
-- [ ] Create a Node class that has properties for the value stored in the node, the left child node, and the right child node.
-- [ ] Create a BinaryTree class
+- [x] Create a Node class that has properties for the value stored in the node, the left child node, and the right child node.
+- [x] Create a BinaryTree class
 - [ ] Define a method for each of the depth first traversals called preOrder, inOrder, and postOrder which returns an array of the values, ordered appropriately.
 - [ ] Any exceptions or errors that come from your code should be semantic, capturable errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
 
-- [ ] Create a BinarySearchTree class
+- [x] Create a BinarySearchTree class
 - [ ] Define a method named add that accepts a value, and adds a new node with that value in the correct location in the binary search tree.
 - [ ] Define a method named contains that accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
 '''
@@ -29,8 +29,8 @@ class BinaryTree():
     info
     '''
 
-    def __init__(self):
-        pass
+    def __init__(self, value=None):
+        self.root = Node(value)
 
     #depth first traversals
     def preOrder(self):
@@ -47,3 +47,27 @@ class BinaryTree():
         pass
         # ordered_list = []
         # return ordered_list
+
+
+class BinaryTreeSearch(BinaryTree):
+    '''
+    info
+    '''
+
+    def add(self, value):
+        node = Node(value)
+        #put it in the correct place
+        if not self.root:
+            self.root = node
+            print("path 1")
+        elif value < self.root.value:
+            self.root.left_node = node
+            print("path 2")
+        else:
+            self.root.right_node = node
+            print("path 3")
+
+    def contains(self, value):
+        # is the value in the tree?
+        # return True/False
+        pass
