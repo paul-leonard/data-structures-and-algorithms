34005e872fed50e216ca4d16806c30f9d93e1e4d
paul-leonard <paul.ryan.leonard@gmail.com>
2020-12-16 22:09:43 -0800

    tests and file structure created

---
+++ python/code_challenges/quick_sort/quick_sort.py
@@ -0,0 +1,9 @@
+'''
+Required Features:
+- [ ] create a function which takes in an integer array and returns an array of the same values sorted in ascending order using a "quick sort" algorithm
+'''
+
+def quick_sort(integer_array):
+    ''' input: integer array;  output: integer array of the same values sorted ascendingly;  This function sorts an array in ascending order using a quick sort algorithm. '''
+
+
