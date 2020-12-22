2b4edfb1c3f9ad2e5790b15d275afc94fc6f01d9
paul-leonard <paul.ryan.leonard@gmail.com>
2020-12-22 08:51:04 -0800

    files connected, imports declared

--- python/tests/test_repeated_word.py
+++ python/tests/test_repeated_word.py
@@ -10,9 +10,10 @@ import pytest
 from code_challenges.repeated_word.repeated_word import first_repeated_word
 
 def test_connection():
-    actual = first_repeated_word()
+    actual = first_repeated_word
     return actual
 
+@pytest.mark.skip("pending")
 def test_same_word_twice():
     actual = first_repeated_word("hello hello")
     expected = "hello"
@@ -38,16 +39,19 @@ def test_integer():
     expected = None
     assert actual == expected
 
+@pytest.mark.skip("pending")
 def test_provided_case_one():
     actual = first_repeated_word("Once upon a time, there was a brave princess who...")
     expected = "a"
     assert actual == expected
 
+@pytest.mark.skip("pending")
 def test_provided_case_two():
     actual = first_repeated_word("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...")
     expected = "it"
     assert actual == expected
 
+@pytest.mark.skip("pending")
 def test_provided_case_three():
     actual = first_repeated_word("It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...")
     expected = "summer"
