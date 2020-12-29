8dafd8684f698542de9e0d138f2a69fb3b0bd159
paul-leonard <paul.ryan.leonard@gmail.com>
2020-12-29 15:25:35 -0800

    all tests passing

--- python/code_challenges/multi_bracket_validation/multi_bracket_validation.py
+++ python/code_challenges/multi_bracket_validation/multi_bracket_validation.py
@@ -1,6 +1,6 @@
 '''
 Required Features:
-- [ ] Your function should take a string as its only argument, and should return a boolean representing whether or not the brackets in the string are balanced. There are 3 types of brackets:
+- [x] Your function should take a string as its only argument, and should return a boolean representing whether or not the brackets in the string are balanced. There are 3 types of brackets:
 Round Brackets : ()
 Square Brackets : []
 Curly Brackets : {}
