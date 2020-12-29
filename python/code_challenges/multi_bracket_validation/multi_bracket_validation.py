1d9178a641bf107fe7631462292f9dcd53632f84
paul-leonard <paul.ryan.leonard@gmail.com>
2020-12-29 15:04:42 -0800

    files created, tests written, tests connected with 1 test passing

---
+++ python/code_challenges/multi_bracket_validation/multi_bracket_validation.py
@@ -0,0 +1,16 @@
+'''
+Required Features:
+- [ ] Your function should take a string as its only argument, and should return a boolean representing whether or not the brackets in the string are balanced. There are 3 types of brackets:
+Round Brackets : ()
+Square Brackets : []
+Curly Brackets : {}
+'''
+
+from stacks_and_queues.stacks_and_queues import Stack
+
+def multi_bracket_validation(input):
+    '''
+    Input: string  Output: boolean stating True if brackets are balanced in the input string
+    '''
+
+    pass
---
+++ python/tests/test_multi_bracket_validation.py
@@ -0,0 +1,74 @@
+'''
+Required Tests:
+- [ ] “Happy Path” - Expected outcome
+- [ ] Expected failure
+- [ ] Edge Case (if applicable/obvious)
+'''
+
+import pytest
+
+from code_challenges.multi_bracket_validation.multi_bracket_validation import multi_bracket_validation
+
+def test_connection():
+    actual = multi_bracket_validation
+    return actual
+
+@pytest.mark.skip("pending")
+def test_no_brackets():
+    actual = multi_bracket_validation("hello hello")
+    expected = True
+    assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_empty_string():
+    actual = multi_bracket_validation("")
+    expected = True
+    assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_provided_case_1():
+    actual = multi_bracket_validation("{}")
+    expected = True
+    assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_provided_case_2():
+    actual = multi_bracket_validation("{}(){}")
+    expected = True
+    assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_provided_case_3():
+    actual = multi_bracket_validation("()[[Extra Characters]]")
+    expected = True
+    assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_provided_case_4():
+    actual = multi_bracket_validation("(){}[[]]")
+    expected = True
+    assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_provided_case_5():
+    actual = multi_bracket_validation("{}{Code}[Fellows](())")
+    expected = True
+    assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_provided_case_6():
+    actual = multi_bracket_validation("[({}]")
+    expected = False
+    assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_provided_case_7():
+    actual = multi_bracket_validation("(](")
+    expected = False
+    assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_provided_case_8():
+    actual = multi_bracket_validation("{(})")
+    expected = False
+    assert actual == expected
